6d081021c2c70f93e25c01637b3d1f045223c4cc
Cay <cay.henning@proton.me>
2024-06-19 07:12:56 -0400

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    base: process.env.BASE_PATH || '/',
+export default defineConfig((mode) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        base: env.VITE_BASE_PATH || '/',
+    }
 });
